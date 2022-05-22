9381c7d9ac7805a756ae72a361661e99b3539fc3
Nikita Shevchik <shevchik.nikita@gmail.com>
2022-05-22 13:54:54 +0300

    discriminant

--- js/scrypt.js
+++ js/scrypt.js
@@ -4,6 +4,9 @@ const quadraticA = document.querySelector(".quadratic__a");
 const quadraticB = document.querySelector(".quadratic__b");
 const quadraticC = document.querySelector(".quadratic__c");
 const quadraticButton = document.querySelector(".quadratic__button");
+const discriminant = document.querySelector(".roots__discriminant")
+const firstRoot = document.querySelector(".roots__first");
+const secondRoot = document.querySelector(".roots__second")
 
 quadraticButton.addEventListener("click", function (e) {
     var a = quadraticA.value;
@@ -12,6 +15,5 @@ quadraticButton.addEventListener("click", function (e) {
 
     var D = (b * b) - 4 * (a * c);
     console.log(D);
-
-
+    discriminant.innerHTML = `Корень дискриминанта = ${D}`;
 });
